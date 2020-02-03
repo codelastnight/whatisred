568c6f47f532622470fc042764d9a8e92006e3e7
not simon <fujizhang2525@gmail.com>
2020-02-03 01:55:48 -0800

    inital per 2

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
 	},
 	output: {
 		filename: '[name].js',
-		path: path.resolve(__dirname, 'dist') // Compile into a folder called "dist"
+		path: path.resolve(__dirname, 'docs') // Compile into a folder called "dist"
 	},
 
 	plugins: [
